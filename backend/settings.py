cb8059b7c483ea50d5dd66d3c7e0d9be57439963
OpenAst <isrealme0@gmail.com>
2024-12-03 15:32:36 +0100

    on it

--- backend/settings.py
+++ backend/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = 'django-insecure-*0q1pwh9o8h&-pbk49kl6q==kr6-^4h&l*)a@z1nd7w$=*4hkb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
