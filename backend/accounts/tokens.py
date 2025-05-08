2677859535bbfedf922619eb7459e5141d5880bc
OpenAst <isrealme0@gmail.com>
2025-05-08 23:56:11 +0100

    modified3

--- backend/accounts/tokens.py
+++ backend/accounts/tokens.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
 from django.utils import timezone
+from django.utils.http import base36_to_int
+from django.utils.crypto import constant_time_compare
 from datetime import timedelta
 
 class OneDayActivationTokenGenerator(PasswordResetTokenGenerator):
@@ -7,13 +9,21 @@ class OneDayActivationTokenGenerator(PasswordResetTokenGenerator):
     if not super().check_token(user, token):
       return False
     
-    ts_built = self._parse_timestamp(token)
-
-    created_time = self._num_seconds(ts_built)
+    try:
+      ts_b36 = token.split("-")[1]
+      ts_int = base36_to_int(ts_b36)
+    except (IndexError, ValueError):
+      return False
+    
+    token_time = self._date_from_timestamp(ts_int)
     now_time = timezone.now()
 
-    if (now_time - created_time) > timedelta(hours=24):
+    if (now_time - token_time) > timedelta(hours=24):
       return False
     
     return True
+  
+  def _date_from_timestamp(self, ts):
+    from datetime import datetime
+    return datetime(2000, 1, 1, tzinfo=timezone.utc) + timedelta(seconds=ts * 60)
   
