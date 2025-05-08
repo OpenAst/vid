760121a67b9c3f6618d4dbf9f0863293501c19c2
OpenAst <isrealme0@gmail.com>
2025-05-09 00:34:22 +0100

    modified5

--- backend/accounts/tokens.py
+++ backend/accounts/tokens.py
@@ -1,31 +1,29 @@
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
-from django.utils import timezone
-from django.utils.http import base36_to_int
-from django.utils.crypto import constant_time_compare
-from datetime import timedelta, datetime
+from datetime import datetime, timedelta, timezone  
 
 class OneDayActivationTokenGenerator(PasswordResetTokenGenerator):
-    def check_token(self, user, token):
-        if not super().check_token(user, token):
-            return False
-
-        
-        ts = self._get_timestamp(token)
-
-        token_created_time = self._date_from_timestamp(ts)
-        now = timezone.now()
-
-        if now - token_created_time > timedelta(hours=24):
-            return False
-
-        return True
-
     def _get_timestamp(self, token):
         try:
             ts_b36 = token.split("-")[1]
             return int(ts_b36, 36)
         except (IndexError, ValueError):
-            return 0
+            return None
 
     def _date_from_timestamp(self, ts):
         return datetime(2001, 1, 1, tzinfo=timezone.utc) + timedelta(seconds=ts * 60)
+
+    def check_token(self, user, token):
+        if not super().check_token(user, token):
+            return False
+
+        ts = self._get_timestamp(token)
+        if ts is None:
+            return False
+
+        token_time = self._date_from_timestamp(ts)
+        now = datetime.now(timezone.utc)
+
+        if (now - token_time) > timedelta(hours=24):
+            return False
+
+        return True
