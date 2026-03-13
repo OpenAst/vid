163201e96c454f4540d49e8e55baa18ca3bc6f2e
OpenAst <isrealme0@gmail.com>
2026-03-13 21:44:46 +0100

    Fix asgi.py import order and call django.setup()

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -1,11 +1,13 @@
 import os
+import django
 from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+django.setup()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from video.routing import websocket_urlpatterns
 from video.ws_auth import QueryStringJWTAuthMiddlewareStack
-
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
@@ -13,3 +15,4 @@ application = ProtocolTypeRouter({
         URLRouter(websocket_urlpatterns)
     ),
 })
+Line:1: 
