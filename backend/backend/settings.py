9e03bc8cedf7dec2a355cec62395fbbedd9781e0
OpenAst <isrealme0@gmail.com>
2026-04-24 18:28:41 +0100

    redis

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -132,7 +132,7 @@ else:
     ))
 
     SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
-    SECURE_SSL_REDIRECT = True
+    SECURE_SSL_REDIRECT = config("SECURE_SSL_REDIRECT", default=False, cast=bool)
 
 
 # ----------------------------
